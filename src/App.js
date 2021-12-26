053f1b2f90ef18ea1096a97aa6b91f495c809781
lalit kumar <lalit.kumar@wesionenroute.com>
2021-12-26 19:13:19 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useParams,
+} from 'react-router-dom';
+
+const Topic = (props) => {
+  const param = useParams();
+  return <div>{param.topicId}</div>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/shop/:topicId" element={<Topic />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
